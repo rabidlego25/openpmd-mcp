90241dcdf41d2f2256359e4c2828339e17bb513b
tabbi31 <tabbi31@rosi4.host>
2026-05-17 22:32:05 +0200

    Added discovery and find_series tools. Fixed Bugs and tested tools.

--- server.py
+++ server.py
@@ -6,12 +6,16 @@ from mcp.server.fastmcp import FastMCP
 from tools.structure import register_structure_tools
 from tools.runs import register_run_tools
 from tools.logs import register_log_tools
+from tools.scheduler import register_scheduler_tools
+from tools.discovery import register_discovery_tools
 
 mcp = FastMCP('openpmd-inspector')
 
 register_structure_tools(mcp)
 register_run_tools(mcp)
 register_log_tools(mcp)
+register_scheduler_tools(mcp)
+register_discovery_tools(mcp)
 
 if __name__ == '__main__':
     mcp.run()
